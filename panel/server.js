e94c9820850673d70a589f347c48b719787131f3
EC2 Default User <ec2-user@stats.eggdropapp.com>
2014-01-28 20:23:04 +0000

    Updating packages, making it all dynamic, persisting room data in mongo, and starting on check ins

--- panel/server.js
+++ panel/server.js
@@ -6,6 +6,9 @@
 
 var socket = require('socket.io');
 var mqtt = require('mqtt');
+var databaseUrl = "huddlebuddy";
+var collections = ["checkins","rooms"];
+var db = require("mongojs").connect(databaseUrl, collections);
 var io = socket.listen(3000);
 var mqttbroker = 'localhost';
 var mqttport = 1883;
@@ -14,19 +17,47 @@ var mqttport = 1883;
 var mqttclient = mqtt.createClient(mqttport, mqttbroker);
 
 // Reduce socket.io debug output
-io.set('log level', 0)
+io.set('log level', 1)
 
 // Subscribe to topic
 io.sockets.on('connection', function (socket) {
     socket.on('subscribe', function (data) {
         mqttclient.subscribe(data.topic);
     });
+    socket.on('checkIn', function (data) {
+        db.checkins.save({room:data.room,user:data.user});
+    });
+    
+    db.rooms.find({},function (err,result) {
+    	result.forEach(function(err,room){
+    		io.sockets.emit('mqtt',
+		        {'topic'  : result[room].room,
+		         'payload' : result[room].status
+		        }
+		    );
+    	});
+    })
+    db.checkins.find({},function (err,result) {
+    	result.forEach(function(err,checkin){
+    		io.sockets.emit('checkin',
+		        {'room'  : result[checkin].room,
+		         'user' : result[checkin].user
+		        }
+		    );
+    	});
+    })
 });
 
 // Push the message to socket.io
 mqttclient.on('message', function(topic, payload) {
+	var room = topic.split("/")[1];
+	if(payload == false) {
+		// Wipe the room
+		db.checkins.remove({room: room});
+	}
+	db.rooms.update({room: room}, {$set:{status:payload}}, {upsert:true});
     io.sockets.emit('mqtt',
-        {'topic'  : topic,
+        {'topic'  : room,
          'payload' : payload
         }
     );
