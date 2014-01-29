1b1db9f77f7409e2d9a9596a18828261e3bcd262
EC2 Default User <ec2-user@stats.eggdropapp.com>
2014-01-29 22:42:50 +0000

    Checking in to vacant rooms reserves it for until it gets caught by inactivity

--- panel/server.js
+++ panel/server.js
@@ -15,7 +15,7 @@ var mqttport = 1883;
 //var mqttbroker = 'localhost';
 //var mqttport = '1883';
 var mqttclient = mqtt.createClient(mqttport, mqttbroker);
-
+mqttclient.subscribe("rooms/#");
 // Reduce socket.io debug output
 io.set('log level', 0)
 
@@ -23,11 +23,11 @@ io.set('log level', 0)
 function app_loop(){
    setTimeout(function(){
         var time = Math.round(Date.now()/1000);
-        db.rooms.find({}, function(err,result) {
+        db.rooms.find({flag:{$ne:0}}, function(err,result) {
             result.forEach(function(err,room){
-                if(time - result[room].time > 600) {
+                if(time - result[room].time > 600 && result[room].flag > 0) {
                     updateRoom(result[room].room, 0);
-                } else if(time - result[room].time > 300) {
+                } else if(time - result[room].time > 300 && result[room].flag == 2) {
                     updateRoom(result[room].room, 1);
                 }
             });
@@ -43,16 +43,20 @@ function updateRoom(room, flag) {
         db.checkins.remove({room: room});
     }
     var time = Math.round(Date.now()/1000);
-    db.logs.insert({type:"room_update",room: room, flag: flag, time: time});
-    if(flag == 2) 
-        db.rooms.update({room: room}, {$set:{flag:flag, time: time}}, {upsert:true});
-    else
-        db.rooms.update({room: room}, {$set:{flag:flag}}, {upsert:true});
-    io.sockets.emit('mqtt',
-        {'topic'  : room,
-         'flag' : flag
+    db.rooms.findOne({room: room}, function(err,result) {
+        if(result.flag != flag) {
+            db.logs.insert({type:"room_update",room: room, flag: flag, time: time});
         }
-    );
+        if(flag == 2) 
+            db.rooms.update({room: room}, {$set:{flag:flag, time: time}}, {upsert:true});
+        else
+            db.rooms.update({room: room}, {$set:{flag:flag}}, {upsert:true});
+        io.sockets.emit('mqtt',
+            {'topic'  : room,
+             'flag' : flag
+            }
+        );
+    })
 }
 
 // Subscribe to topic
@@ -65,40 +69,48 @@ io.sockets.on('connection', function (socket) {
         var time = Math.round(Date.now()/1000);
         db.logs.insert({type:"check_in", room: data.room, user: data.user, time: time});
         io.sockets.emit('checkin',
-	        {'room'  : data.room,
-	         'user' : data.user,
-	         'client_id' : data.client_id
-	        }
-	    );
+            {'room'  : data.room,
+             'user' : data.user,
+             'client_id' : data.client_id
+            }
+        );
+        mqttclient.publish('rooms/'+data.room, 'true');
     });
-    
+    socket.on('requestLogs', function (data) {
+        var pastDay = Math.round(Date.now()/1000)-86400;
+        db.logs.find({time : {$gt:pastDay}}, function(err,result) {
+            io.sockets.socket(socket.id).emit('logs',
+                result
+            );
+        });
+    });
     db.rooms.find({},function (err,result) {
-    	result.forEach(function(err,room){
-    		io.sockets.emit('mqtt',
-		        {'topic'  : result[room].room,
-		         'flag' : result[room].flag
-		        }
-		    );
-    	});
+        result.forEach(function(err,room){
+            io.sockets.socket(socket.id).emit('mqtt',
+                {'topic'  : result[room].room,
+                 'flag' : result[room].flag
+                }
+            );
+        });
     })
     db.checkins.find({},function (err,result) {
-    	result.forEach(function(err,checkin){
-    		io.sockets.emit('checkin',
-		        {'room'  : result[checkin].room,
-		         'user' : result[checkin].user
-		        }
-		    );
-    	});
+        result.forEach(function(err,checkin){
+            io.sockets.socket(socket.id).emit('checkin',
+                {'room'  : result[checkin].room,
+                 'user' : result[checkin].user
+                }
+            );
+        });
     })
 });
 
 // Push the message to socket.io
 mqttclient.on('message', function(topic, payload) {
-	var room = topic.split("/")[1];
-	if(payload == "false") {
+    var room = topic.split("/")[1];
+    if(payload == "false") {
         // This is just kept for testing
         var flag = 0;
-	} else {
+    } else {
         var flag = 2;
     }
     updateRoom(room, flag);
